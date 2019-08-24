86382e15580c2226180e284c9928bd5a7d83de5c
Jacob Killelea <jkillelea@protonmail.ch>
2019-08-23 19:43:19 -0700

    change '//' for '/* ... */'

--- fsw/src/gps_kalman_utils.c
+++ fsw/src/gps_kalman_utils.c
@@ -31,12 +31,12 @@
 
 
 /* convert from DDDMM.mmmmm (decimal minutes) to DDD.dddddd (plain decimal) format */
-// TODO: double check this!
+/* TODO: double check this! */
 double decimal_minutes2decimal_decimal(const double decimal_minutes) {
-    double degrees = ((int) (decimal_minutes/100.0)); // DDD
-    double minutes = decimal_minutes - 100*degrees;   // MM.mmmmmm
-    double decimal = minutes / 60;                    // 0.ddddddd
-    return (degrees + decimal);                       // DDD.dddddd
+    double degrees = ((int) (decimal_minutes/100.0)); /* DDD */
+    double minutes = decimal_minutes - 100*degrees;   /* MM.mmmmmm */
+    double decimal = minutes / 60;                    /* 0.ddddddd */
+    return (degrees + decimal);                       /* DDD.dddddd */
 }
 
 
